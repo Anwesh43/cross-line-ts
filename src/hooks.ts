48278f28609cd30e0502b7e25629b822f059bf90
Anwesh43 <anweshthecool0@gmail.com>
2021-05-16 19:26:53 +0530

    adding useDimension hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -3,7 +3,7 @@ import {useState, useEffect} from 'react'
 const scGap : number = 0.02 
 const delay : number = 20 
 
-const useAnimatedScale = () => {
+export const useAnimatedScale = () => {
     const [scale, setScale] = useState(0)
     const [animated, setAnimated] = useState(false)
     return {
@@ -23,5 +23,23 @@ const useAnimatedScale = () => {
                 }, delay)
             }
         }
+    }
+}
+
+export const useDimesnion = () => {
+    const [w, setW] = useState(window.innerWidth)
+    const [h, setH] = useState(window.innerHeight)
+    useEffect(() => {
+        window.onresize = () => {
+            setW(window.innerWidth)
+            setH(window.innerHeight)
+        }
+        return () => {
+            
+        }
+    })
+    return {
+        w,
+        h, 
     }
 }
