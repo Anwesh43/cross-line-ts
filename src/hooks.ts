d25b068fd6964662ae8cfdde35ba9a25b4e16f87
Anwesh43 <anweshthecool0@gmail.com>
2021-05-16 19:41:18 +0530

    adding useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, CSSProperties} from 'react'
 
 const scGap : number = 0.02 
 const delay : number = 20 
@@ -35,11 +35,46 @@ export const useDimesnion = () => {
             setH(window.innerHeight)
         }
         return () => {
-            
+
         }
     })
     return {
         w,
         h, 
+    }
+}
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    const position = 'absolute'
+    const x = w / 2
+    const y = h / 2
+    const size = Math.min(w, h) / 10 
+    return {
+        crossStyle(i : number) : CSSProperties {
+            const top = `${y}px`
+            const left = `${x}px`
+            const height = `${size * scale}px`
+            const width = `${Math.min(w, h) / 90}px`
+            const transform = `rotate(${90 * i + 45}deg)`
+            const background = 'indigo'
+            return {
+                position, 
+                top, 
+                left, 
+                width, 
+                height, 
+                transform,
+                background 
+            }
+        },
+        buttonStyle() {
+            const left = `${w * 0.35}px`
+            const top = `${h * 0.8}px`
+            return {
+                position, 
+                left, 
+                top 
+            }
+        }
     }
 }
